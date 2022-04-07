63a88aba3bd9c268536db0f64b774107a79aad5a
Shaun Inman <shaun@panic.com>
2022-04-06 21:39:20 -0400

    updated 8.3 toolchain

--- support/setup-buildroot.sh
+++ support/setup-buildroot.sh
@@ -2,8 +2,11 @@
 
 cd ~
 
+TOOLCHAIN_VERSION=v0.0.2
 TOOLCHAIN_NAME="gcc-arm-8.3-2019.03-aarch64-arm-linux-gnueabihf"
-wget https://github.com/shauninman/gcc-arm-8.3-aarch64/releases/download/v0.0.1/$TOOLCHAIN_NAME.tar.xz
+if [ ! -f "$TOOLCHAIN_NAME.tar.xz" ]; then
+	wget https://github.com/shauninman/gcc-arm-8.3-aarch64/releases/download/$TOOLCHAIN_VERSION/$TOOLCHAIN_NAME.tar.xz
+fi
 tar -xf ./$TOOLCHAIN_NAME.tar.xz
 # don't delete this tar!
 
